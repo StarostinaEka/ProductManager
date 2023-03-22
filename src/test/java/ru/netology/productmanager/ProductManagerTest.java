e267d7f6acc11a910e626834b465c9befa8fa9c8
Eka Starostina <starostinaeka@gmail.com>
2023-03-22 17:07:00 +0300

    Add tests

--- src/test/java/ru/netology/productmanager/ProductManagerTest.java
+++ src/test/java/ru/netology/productmanager/ProductManagerTest.java
@@ -11,9 +11,9 @@ class ProductManagerTest {
     void testAdd() {
         ProductRepo repo = new ProductRepo();
         ProductManager manager = new ProductManager(repo);
-        Book book1 = new Book(1, "Harry Potter 1", 100, "Rowling" );
-        Book book2 = new Book(2, "Harry Potter 2", 300, "Rowling" );
-        Book book3 = new Book(3, "War and Peace", 500, "Tolstoy" );
+        Book book1 = new Book(1, "Harry Potter 1", 100, "Rowling");
+        Book book2 = new Book(2, "Harry Potter 2", 300, "Rowling");
+        Book book3 = new Book(3, "War and Peace", 500, "Tolstoy");
 
         manager.add(book1);
         manager.add(book2);
@@ -30,9 +30,9 @@ class ProductManagerTest {
     void testSearchByWhenFewProductsFind() {
         ProductRepo repo = new ProductRepo();
         ProductManager manager = new ProductManager(repo);
-        Book book1 = new Book(1, "Harry Potter 1", 100, "Rowling" );
-        Book book2 = new Book(2, "Harry Potter 2", 300, "Rowling" );
-        Book book3 = new Book(3, "War and Peace", 500, "Tolstoy" );
+        Book book1 = new Book(1, "Harry Potter 1", 100, "Rowling");
+        Book book2 = new Book(2, "Harry Potter 2", 300, "Rowling");
+        Book book3 = new Book(3, "War and Peace", 500, "Tolstoy");
 
         manager.add(book1);
         manager.add(book2);
@@ -40,6 +40,43 @@ class ProductManagerTest {
 
         Product[] actual = manager.searchBy("Potter");
         Product[] expected = {book1, book2};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void testSearchByWhenNoProductsFind() {
+        ProductRepo repo = new ProductRepo();
+        ProductManager manager = new ProductManager(repo);
+        Book book1 = new Book(1, "Harry Potter 1", 100, "Rowling");
+        Book book2 = new Book(2, "Harry Potter 2", 300, "Rowling");
+        Book book3 = new Book(3, "War and Peace", 500, "Tolstoy");
+
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(book3);
+
+        Product[] actual = manager.searchBy("Hamlet");
+        Product[] expected = {0};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    void testSearchByWhenOneProductsFind() {
+        ProductRepo repo = new ProductRepo();
+        ProductManager manager = new ProductManager(repo);
+        Book book1 = new Book(1, "Harry Potter 1", 100, "Rowling");
+        Book book2 = new Book(2, "Harry Potter 2", 300, "Rowling");
+        Book book3 = new Book(3, "War and Peace", 500, "Tolstoy");
+
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(book3);
+
+        Product[] actual = manager.searchBy("Peace");
+        Product[] expected = {book3};
 
         Assertions.assertArrayEquals(expected, actual);
     }
